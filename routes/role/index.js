6e57461bc560e62c5924bd85578d85920b23d657
ojokne <ojokne@gmail.com>
2022-12-17 14:53:54 +0300

    add delete delete route to role route handler

--- routes/role/index.js
+++ routes/role/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const add = require("./add");
 const app = express.Router();
+const deleteRole = require("./delete");
 
 app.use(add);
+app.use(deleteRole);
 module.exports = app;
