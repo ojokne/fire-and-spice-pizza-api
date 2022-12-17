c9ef4658da43da09d6975e3783c452cb14788ca4
ojokne <ojokne@gmail.com>
2022-12-17 11:27:22 +0300

    Add users.js
    This route returns all users

--- routes/user/index.js
+++ routes/user/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const add = require("./add");
 const update = require("./update");
+const users = require("./users");
 const app = express.Router();
 app.use(add);
 app.use(update);
+app.use(users);
 module.exports = app;
