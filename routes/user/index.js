248455be2a2fc0a61e4a4d72a569557e3c054f89
ojokne <ojokne@gmail.com>
2022-12-17 10:38:32 +0300

    Add update.js
    Add update function to routes

--- routes/user/index.js
+++ routes/user/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const add = require("./add");
+const update = require("./update");
 const app = express.Router();
 app.use(add);
-
+app.use(update);
 module.exports = app;
