c9b36292cee25d7fd4cec051169bafc0618e2ba9
ojokne <ojokne@gmail.com>
2022-12-17 14:12:00 +0300

    Check if role exists
    Check if the role provided is already created

--- routes/role/add.js
+++ routes/role/add.js
@@ -6,15 +6,34 @@ const app = express.Router();
 app.route("/add").post(async (req, res) => {
   response_code = 1;
   response_message = "successfully created";
-  if (!req.body.hasOwnProperty("roleName")) {
+  if (
+    !(
+      req.body.hasOwnProperty("roleNumber") &&
+      req.body.hasOwnProperty("roleName")
+    )
+  ) {
     response_message = " Add all fields";
     response_code = 0;
   } else {
-    if (req.body.roleName.trim().length < 1) {
+    if (
+      req.body.roleNumber.trim().length < 1 ||
+      req.body.roleName.trim().length < 1
+    ) {
       response_message = " populate all fields";
       response_code = 2;
     } else {
-      const role = Role.create({ roleName: "Admin" });
+      let roleReturned = await Role.findByPk(
+        parseInt(req.body.roleNumber.trim())
+      );
+      if (roleReturned) {
+        response_message = "Resource already exists";
+        response_code = 5;
+      } else {
+        const role = await Role.create({
+          roleNumber: parseInt(req.body.roleNumber.trim()),
+          roleName: req.body.roleName.trim(),
+        });
+      }
     }
   }
   res.json({ response_code, response_message });
