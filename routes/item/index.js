b0b9188b9c420d04581c04ae2c8ba82a35115e31
ojokne <ojokne@gmail.com>
2022-12-18 11:43:16 +0300

    Create index.js
    Route handler for items

---
+++ routes/item/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const add = require("./add");
+// const update = require("./update");
+// const items = require("./users");
+// const item = require("./user");
+// const deleteItem = require("./delete");
+const app = express.Router();
+
+app.use(add);
+// app.use(update);
+// app.use(users);
+// app.use(user);
+// app.use(deleteUser);
+module.exports = app;
