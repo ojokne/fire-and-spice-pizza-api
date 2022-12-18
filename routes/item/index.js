d646b25ee9a644ca8901f38619757604ab3ab658
ojokne <ojokne@gmail.com>
2022-12-18 11:55:11 +0300

    Add update.js to item route handler
    Update.js contains routes to update items

--- routes/item/index.js
+++ routes/item/index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const add = require("./add");
-// const update = require("./update");
+const update = require("./update");
 // const items = require("./users");
 // const item = require("./user");
 // const deleteItem = require("./delete");
 const app = express.Router();
 
 app.use(add);
-// app.use(update);
+app.use(update);
 // app.use(users);
 // app.use(user);
 // app.use(deleteUser);
