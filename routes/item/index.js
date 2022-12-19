6065ae74b89e2fda18d9f4ef2b3f1a8de39efcaa
ojokne <ojokne@gmail.com>
2022-12-19 21:45:48 +0300

    Add route to return items

--- routes/item/index.js
+++ routes/item/index.js
@@ -2,13 +2,13 @@ const express = require("express");
 const add = require("./add");
 const update = require("./update");
 const deleteItem = require("./delete");
-// const items = require("./users");
+const items = require("./items");
 // const item = require("./user");
 const app = express.Router();
 
 app.use(add);
 app.use(update);
 app.use(deleteItem);
-// app.use(users);
+app.use(items);
 // app.use(user);
 module.exports = app;
