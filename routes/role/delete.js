412294db633a6cc100b7764bfa19727f261461f5
ojokne <ojokne@gmail.com>
2022-12-19 21:34:04 +0300

    Remove check for req.params
    Code runs without need for it

--- routes/role/delete.js
+++ routes/role/delete.js
@@ -6,23 +6,16 @@ const app = express.Router();
 app.route("/:roleNumber").delete(async (req, res) => {
   let response_code = 6;
   let response_message = "successfully deleted";
-  if (!req.params.hasOwnProperty("roleNumber")) {
-    response_message = " Add all fields";
-    response_code = 0;
+
+  const roleReturned = await Role.findByPk(req.params.roleNumber);
+  if (roleReturned) {
+    await roleReturned.destroy();
+    roleReturned.save();
   } else {
-    if (req.params.roleNumber.trim().length < 1) {
-      response_message = " populate all fields";
-      response_code = 2;
-    } else {
-      const roleReturned = await Role.findByPk(req.params.roleNumber.trim());
-      if (roleReturned) {
-        await roleReturned.destroy(), roleReturned.save();
-      } else {
-        response_message = "Not found";
-        response_code = 3;
-      }
-    }
+    response_message = "Not found";
+    response_code = 3;
   }
+
   res.send({ response_code, response_message });
 });
 module.exports = app;
