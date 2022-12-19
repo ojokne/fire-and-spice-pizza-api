5f36acc698b651b741d2eef46735851f32b5d1b2
ojokne <ojokne@gmail.com>
2022-12-19 21:45:31 +0300

    Create route to return items
    Route to return items

---
+++ routes/item/items.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const Item = require("../../models/Item");
+const app = express.Router();
+
+app.route("/").get(async (req, res) => {
+  const list = await Item.findAll();
+  const items = new Array(list.length);
+  for (let i = 0; i < list.length; i++) {
+    let item = {
+      itemNumber: list[i].itemNumber,
+      itemName: list[i].itemName,
+      unitCost: list[i].unitCost,
+    };
+    items[i] = item;
+  }
+
+  res.send(items);
+});
+
+module.exports = app;
