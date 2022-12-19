80316cd648a6416bccc59686d7dcd329506c8721
ojokne <ojokne@gmail.com>
2022-12-19 21:34:55 +0300

    Add delete route handler

--- routes/item/index.js
+++ routes/item/index.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const add = require("./add");
 const update = require("./update");
+const deleteItem = require("./delete");
 // const items = require("./users");
 // const item = require("./user");
-// const deleteItem = require("./delete");
 const app = express.Router();
 
 app.use(add);
 app.use(update);
+app.use(deleteItem);
 // app.use(users);
 // app.use(user);
-// app.use(deleteUser);
 module.exports = app;
