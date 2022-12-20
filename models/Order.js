59b19fecfcac6df9f0ecba7b4ce0dd42520b94fd
ojokne <ojokne@gmail.com>
2022-12-20 21:33:44 +0300

    Change primary key type
    Make primary key type Integer and an autoincrement

--- models/Order.js
+++ models/Order.js
@@ -4,12 +4,14 @@ const OrderedItem = require("./OrderedItem");
 
 const Order = db.sequelize.define("Order", {
   orderNumber: {
-    type: DataTypes.STRING,
+    type: DataTypes.INTEGER,
     primaryKey: true,
+    autoIncrement: true,
     allowNull: false,
   },
   orderStatus: {
     type: DataTypes.BOOLEAN,
+    defaultValue: false,
   },
   table: DataTypes.INTEGER,
   deliveredAt: {
