9eee8a03477779bb36e7f6fbb2d39cf84b58e8c2
ojokne <ojokne@gmail.com>
2022-12-21 21:27:45 +0300

    Remove commented code

--- routes/order/index.js
+++ routes/order/index.js
@@ -1,14 +1,10 @@
 const express = require("express");
 const add = require("./add");
-// const update = require("./update");
-// const deleteItem = require("./delete");
 const orders = require("./orders");
 const order = require("./order");
 const app = express.Router();
 
 app.use(add);
-// app.use(update);
-// app.use(deleteItem);
 app.use(orders);
 app.use(order);
 module.exports = app;
